58dd7441ea212cb459859da1e03e5602abfd89e0
mbntex <mbneveu@gmail.com>
2017-10-25 18:35:58 -0700

    started

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-var Search = () => (
+var Search = (props) => (
   <div className="search-bar form-inline">
     <input className="form-control" type="text" />
     <button className="btn hidden-sm-down">
@@ -6,6 +6,7 @@ var Search = () => (
     </button>
   </div> 
 );
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,27 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
+var VideoList = (props) => (
+  
+  <div>
+    { 
+      props.videoData.map((video, i) => 
+        <VideoListEntry key={i}/>
+      )
+    }
+  </div>    
 );
+
+
+  /*<div className="video-list">
+    <div><h5><em>videoListEntry</em> {props.videoData[0].snippet.channelTitle}</h5></div>
+    <div><h5><em>videoListEntry</em> Hello </h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> 
+  </div> */
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  videoData: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
